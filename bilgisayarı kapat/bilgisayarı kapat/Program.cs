ab6243e7c1faa5947278d740552ccc35d41c1080
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2022-02-21 15:53:47 +0300

    .

---
+++ bilgisayarı kapat/bilgisayarı kapat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;//bu kütüphane eklenmelidir.
+
+namespace bilgisayarı_kapat
+{
+    class Program
+    {
+
+        static void kapat()
+        {
+            Process.Start("shutdown", "/s /t 0");//bilgisayarı kapatır
+        }
+
+        static void Main(string[] args)
+        {
+            kapat();
+        }
+    }
+}
